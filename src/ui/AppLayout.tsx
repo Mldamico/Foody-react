73f05ec6b2dab2661c5129ee89e46bf3ba082ae5
mldamico <matias.lioneldamico@gmail.com>
2023-07-06 20:45:16 -0300

    Layout and header

---
+++ src/ui/AppLayout.tsx
@@ -0,0 +1,12 @@
+import { Header } from "./Header";
+
+export const AppLayout = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Content</h1>
+      </main>
+    </div>
+  );
+};
